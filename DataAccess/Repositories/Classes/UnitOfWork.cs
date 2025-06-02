70a83764591af68fb46096c0e54d7dab35c2c2a3
Sci Alex <sim.AdhamZakaria23024@alexu.edu.eg>
2025-06-02 21:42:39 +0300

    Repositories and Unit of wok

---
+++ DataAccess/Repositories/Classes/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Contexts;
+using DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Classes
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Lazy<IAuthorRepository> _authorRepository;
+        private readonly Lazy<IBookRepository> _bookRepository;
+
+        private readonly LibraryDbContext _libraryDbContext;
+
+        public UnitOfWork(LibraryDbContext libraryDbContext )
+        {
+            _libraryDbContext = libraryDbContext;
+            _authorRepository = new Lazy<IAuthorRepository>(() => new AuthorRepository(_libraryDbContext));
+            _bookRepository = new Lazy<IBookRepository>(() => new BookRepository(_libraryDbContext));
+        }
+
+        public IAuthorRepository AuthorRepository => _authorRepository.Value;
+        public IBookRepository BookRepository => _bookRepository.Value;
+
+        public int SaveChanges()
+        {
+            return _libraryDbContext.SaveChanges();
+        }
+    }
+}
---
+++ DataAccess/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate);
+    }
+}
